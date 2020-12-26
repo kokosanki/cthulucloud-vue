3fc2c1aa85789585d5844d84e3fb9c7c5cad37e0
kokosanki <os_mo@wp.pl>
2020-12-26 17:26:35 +0100

    homepage basics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home d-flex justify-center pa-5"
+       style="height: calc(100vh - 56px)">
+        <v-btn @click="createCharacter">Create a character</v-btn>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  methods: {
+    createCharacter () {
+      console.log('create')
+    }
   }
 }
 </script>
