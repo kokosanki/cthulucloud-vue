4a7de9ee95fe888366c180f399486f7a095eea17
kokosanki <40806830+kokosanki@users.noreply.github.com>
2020-12-26 17:33:01 +0100

    Revert "homepage basics"

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-    </v-app-bar>
-
-    <v-main>
-      <Home/>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import Home from '@/views/Home.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Home
-  },
-
-  data: () => ({
-    //
-  })
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
